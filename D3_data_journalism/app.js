72e7a1dea724033d165253608f78c608edfefcd4
prasanna0913 <dspprasanna0913@gmail.com>
2021-01-17 01:32:26 -0500

    updating for the 2nd axis

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -49,6 +49,59 @@ function renderAxes(newXScale, xAxis) {
     return xAxis;
 }
 
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenxAxis) {
+
+    circlesGroup.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[chosenxAxis]));
+
+    return circlesGroup;
+}
+
+function renderCirclesText(circlesGroup, newXScale, chosenxAxis) {
+
+    labelGroup.transition()
+        .duration(1000)
+        .attr("x", d => newXScale(d[chosenxAxis]));
+
+    return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenxAxis, circlesGroup) {
+
+    var label;
+
+    if (chosenxAxis === "poverty") {
+        var label = "Poverty:";
+    } else {
+        var label = "Age:"
+    }
+
+    var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([120, -10])
+        //.offset([0,5])
+        .html(function (d) {
+            return (`${d.state}<br>Coverage: ${parseFloat(d.healthcare * 100).toFixed(1)}%<br>${label} ${d[chosenxAxis]}`);
+        });
+
+
+    circlesGroup.call(toolTip);
+
+    circlesGroup.on("mouseover", function (data) {
+        toolTip.show(data, this);
+    })
+        // onmouseout event
+        .on("mouseout", function (dataTip, index) {
+            toolTip.hide(dataTip);
+        });
+
+    return circlesGroup;
+}
+
 // Step 1: Parse Data/Cast as numbers
 // ==============================
 d3.csv("data.csv").then(function (journalismData) {
@@ -79,6 +132,38 @@ d3.csv("data.csv").then(function (journalismData) {
         .classed("x-axis", true)
         .attr("transform", `translate(0, ${height})`)
         .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    // append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(journalismData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d[chosenxAxis]))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", 20)
+        .attr("fill", "red")
+        .attr("opacity", ".5");
+
+    // Step 6: Append text to Created Circles
+    // ==============================
+    labelGroup = chartGroup.append("g")
+        .selectAll("text")
+        .data(journalismData)
+        .enter()
+        .append("text").text(d => d.abbr)
+        .attr("x", d => xLinearScale(d[chosenxAxis]))
+        .attr("y", d => yLinearScale(d.healthcare))
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "central")
+        .attr("font_family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("fill", "blue")
+        .style("font-weight", "bold");
+
 
 
 }).catch(function (error) {
